f9a84afa050006c041d972bd0c0d3c0ce9eeea8a
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-06-19 21:06:45 +0500

    Done

--- Client/src/components/Sidebar.jsx
+++ Client/src/components/Sidebar.jsx
@@ -6,12 +6,16 @@ import { NavLink } from 'react-router-dom';
 import Avatar from "../components/Avatar"
 import { useSelector } from 'react-redux';
 import EdituserDetail from './EdituserDetail';
+import { FiArrowUpLeft } from "react-icons/fi";
+import Search from './Search';
 
 const Sidebar = () => {
     const user = useSelector((state)=>state.user)
     const [edituseropen, setEdituseropen] = useState(false)
+    const [Alluser, setAlluser] = useState([])
+    const [opensearchuser, setOpensearchuser] = useState(false)
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-full grid grid-cols-[48px,1fr]'>
         <div className='bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5 text-slate-600 flex flex-col justify-between'>
             <div>
             <NavLink className={({isActive})=> `cursor-pointer w-12 h-12 flex justify-center items-center hover:bg-slate-200 rounded ${isActive && "bg-slate-200"}`} title='Chat'>
@@ -19,7 +23,7 @@ const Sidebar = () => {
             size={25}/>
             </NavLink>
 
-            <div title='Add user' className='cursor-pointer w-12 h-12 flex justify-center items-center hover:bg-slate-200 rounded'>
+            <div onClick={()=>setOpensearchuser(true)} title='Add user' className='cursor-pointer w-12 h-12 flex justify-center items-center hover:bg-slate-200 rounded'>
              <FaUserPlus size={25}/>
             </div>
             </div>
@@ -35,8 +39,34 @@ const Sidebar = () => {
             </div>
         </div>
 
+
+        <div className='w-full'>
+            <div className='h-16 flex items-center'>
+            <h2 className='text-2xl font-bold p-4 text-slate-800'>Message</h2>
+            </div>
+            <div className='bg-slate-200 p-[0.5px]'></div>
+            <div className="h-[calc(100vh-65px)] overflow-x-hidden overflow-y-auto scroll">
+                {
+                    Alluser.length === 0 && (
+                        <div className='mt-12'>
+                            <div className='flex justify-center items-center text-slate-500 my-4'>
+                                <FiArrowUpLeft size={50}/>
+                            </div>
+                            <p className='text-slate-400 text-lg text-center '>Explore users to start a convarsation with them</p>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+
 {
     edituseropen && <EdituserDetail onClose={()=>setEdituseropen(false)} user={user}/>
+}
+
+{
+    opensearchuser && (
+        <Search onClose={()=>setOpensearchuser(false)}/>
+    )
 }
 
 
--- server/Router/index.js
+++ server/Router/index.js
@@ -5,6 +5,7 @@ import checkPassword from "../Controllers/checkpassword.js";
 import userDeatil from "../Controllers/userDeatil.js";
 import logout from "../Controllers/logout.js";
 import updateuserdetail from "../Controllers/updateuserdetail.js";
+import search from "../Controllers/Search.js";
 
 
 
@@ -21,4 +22,6 @@ router.get("/userdetail", userDeatil)
 router.get("/logout", logout)
 // update user details
 router.post("/updateuserdetail", updateuserdetail)
+// search user
+router.post("/search-user", search)
 export default router
