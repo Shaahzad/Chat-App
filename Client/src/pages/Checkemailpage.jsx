6cf65b8e7e31a3ee399fc09568ad20952dc19618
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-06-26 20:40:41 +0500

    Done

--- Client/src/pages/Checkemailpage.jsx
+++ Client/src/pages/Checkemailpage.jsx
@@ -34,7 +34,7 @@ const Checkemailpage = () => {
       e.stopPropagation()
       
   
-      const url = "https://localhost:5000/api/checkemail"
+      const url = "https://chat-app-server-zeta-liart.vercel.app/api/checkemail"
   
       try {
         const response = await axios.post(url,data)
